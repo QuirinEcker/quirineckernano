dd8f8d99e24af4048892b689c7332e59c2375a23
Ecker Quirin <it170135@EDU.HTL-LEONDING.AC.AT>
2018-11-28 09:42:52 +0100

    scrollAnimations

--- scrollEventsHomePage.js
+++ scrollEventsHomePage.js
@@ -1,1 +1,8 @@
-// JavaScript Document
+function scrollListener() {
+    let scrollPosition = scrollY;
+    let scrollCounter = document.getElementById("scrollCounter");
+    scrollCounter.innerHTML = scrollPosition;
+
+    if(scrollPosition > 684) {
+    }
+}
